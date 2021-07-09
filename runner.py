13f64a5cb1d144b339546922819f05e7e8df9bd9
Santhoshkumar V <shanvsanthosh@gmail.com>
2021-07-09 15:53:53 +0530

    Delete runner.py

--- runner.py
+++
@@ -1,9 +0,0 @@
-class CallbackRunner:
-    def __init__(self, callback=None):
-        self.callback = callback
-        if self.callback is None:
-            self.callback = []
-            
-    def __call__(self, value, score, model):
-        for cal in self.callback:
-            getattr(cal, value)(score, model)
